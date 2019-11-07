b3a17af6801befa0bb5654b5f46a24bf27d486f3
Luigi-Tramparulo <jackluigi@gmail.com>
2019-11-07 12:14:13 +0100

    feat: added button stop and clear

--- src/routes/timer.js
+++ src/routes/timer.js
@@ -1,79 +1,105 @@
 import React from 'react'
 
 class Timer extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { 
-            hour:0,       
-            minutes:0,
-            secs:0
-        }
-    }
-    tickSecs= () =>{        
-        this.setState((precState)=>{
-            return{
-            secs: precState.secs + 1,             
-        }
-        })
-    }
-    tickMin= () =>{        
-        this.setState((precState)=>{
-            return{
-            minutes: precState.minutes + 1,             
-        }
-        })
-    }
-    tickHour= () =>{
-        this.setState((precState)=>{
-            return{
-            minutes: precState.hour + 1,             
-        }
-        })
-    }
-        
-    startWatch(){
-        setInterval(this.tickSecs,1000);
-    //     setInterval(this.tickMin,(60*1000));
-    //     setInterval(this.tickHour,(60000*6));        
-    }
-    stopWatch(){
-        clearInterval(this.startWatch)
-    }
-    componentDidMount() {
-        this.startWatch();
-    }
+	constructor(props) {
+		super(props)
+		this.tick = this.tickSecs.bind(this)
+		this.state = {
+			hour: 0,
+			minutes: 0,
+			secs: 0,
+			stopped: false
+		}
+	}
+	tickSecs = () => {
+		this.setState((precState) => {
+			if (!this.state.stopped) {
+				return {
+					secs: precState.secs + 1,
+				}
+			}
+		})
+	}
+	tickMin = () => {
+		this.setState((precState) => {
+			return {
+				minutes: precState.minutes + 1,
+			}
+		})
+	}
+	tickHour = () => {
+		this.setState((precState) => {
+			return {
+				minutes: precState.hour + 1,
+			}
+		})
+	}
+	toggleWatch = () => {
+		this.setState(() => {
+			if (this.state.stopped) {
+				return {
+					stopped: false,
+				}
+			} else {
+				return {
+					stopped: true
+				}
+			}
+		}
+		)
 
-    componentDidUpdate(){
-        const {secs, minutes}=this.state
-        if(secs>59){
+	}
 
-            this.setState({secs:0})
-            this.tickMin()            
-        }
-        if(minutes>59){
-            this.setState({minutes:0})
-            this.tickHour()
-        }
-         
-    }
+	startWatch() {
+		setInterval(this.tickSecs, 1000);
+
+	}
+
+	clearWatch = () => {
+		this.setState( 
+			{
+				hour: 0,
+				secs: 0,
+				minutes: 0
+			}
+		)
+	}
+
+	componentDidMount() {		
+		this.startWatch();
+	}
 
 
-    render(){
-        const hour=this.state.hour;
-        const minutes = this.state.minutes;
-        const secs= this.state.secs;
 
-        
-        
-        const time = hour + " " + minutes + " "+ secs;
-        return <h2>Fai partire il cronometro da 
-        {time}
-        <button onClick={this.startCount}>Start</button>
-        <button onClick={this.stopWatc}>Stop</button>
-        <button>Resume</button>
-        <button>Clear</button>
-        </h2>
-    }
+
+	componentDidUpdate() {		
+		const { secs, minutes, stopped } = this.state
+		if (secs > 59) {
+
+			this.setState({ secs: 0 })
+			this.tickMin()
+		}
+		if (minutes > 59) {
+			this.setState({ minutes: 0 })
+			this.tickHour()
+		}
+
+
+	}
+
+
+
+	render() {
+		const hour = this.state.hour;
+		const minutes = this.state.minutes;
+		const secs = this.state.secs;
+		const time = hour + " " + minutes + " " + secs;
+		return <h2>
+      {time}
+			<button className="button-crono" onClick={this.toggleWatch}>Start</button>
+			<button className="button-crono" onClick={this.clearWatch}>Clear</button>
+		</h2>
+	}
 
 }
 
