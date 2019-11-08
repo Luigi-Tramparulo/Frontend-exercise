7e7e446e539a0da617511236498faae5cea237c5
Ivan Di Luccio <ivan.diluccio@abstract-technology.com>
2019-11-08 15:42:25 +0100

    chore: .editorconfig file added

--- src/routes/timer.js
+++ src/routes/timer.js
@@ -1,99 +1,99 @@
 import React from 'react'
 
 class Timer extends React.Component {
-	constructor(props) {
-		super(props)
-		
-		this.state = {
-			millisecs:0,			
-			minutes: 0,
-			secs: 0,
-			stopped: false
-		}
-	}
-	tickSecs = () => {
-		this.setState((precState) => {			
-				return {
-					secs: precState.secs + 1,
-				}
-			
-		})
-	}
-	tickMin = () => {
-		this.setState((precState) => {
-			return {
-				minutes: precState.minutes + 1,
-			}
-		})
-	}
-	tickMillisecs = () => {
-		this.setState((precState) => {
-			if (!this.state.stopped) {
-			return {
-				millisecs: precState.millisecs + 1,
-			}
-		}
-		})
-	}
-	toggleWatch = () => this.setState({ stopped: !this.state.stopped})
+  constructor(props) {
+    super(props)
 
-	startWatch() {
-		setInterval(this.tickMillisecs, 10);
+    this.state = {
+      millisecs: 0,
+      minutes: 0,
+      secs: 0,
+      stopped: false
+    }
+  }
+  tickSecs = () => {
+    this.setState((precState) => {
+      return {
+        secs: precState.secs + 1,
+      }
 
-	}
+    })
+  }
+  tickMin = () => {
+    this.setState((precState) => {
+      return {
+        minutes: precState.minutes + 1,
+      }
+    })
+  }
+  tickMillisecs = () => {
+    this.setState((precState) => {
+      if (!this.state.stopped) {
+        return {
+          millisecs: precState.millisecs + 1,
+        }
+      }
+    })
+  }
+  toggleWatch = () => this.setState({ stopped: !this.state.stopped })
 
-	clearWatch = () => {
-		this.setState( 
-			{
-				millisecs: 0,
-				secs: 0,
-				minutes: 0
-			}
-		)
-	}
+  startWatch() {
+    setInterval(this.tickMillisecs, 10);
 
-	componentDidMount() {		
-		this.startWatch();		
-	}
+  }
 
-	componentDidUpdate() {				
-		const { millisecs,secs, minutes, stopped } = this.state
-		if (secs > 59) {
+  clearWatch = () => {
+    this.setState(
+      {
+        millisecs: 0,
+        secs: 0,
+        minutes: 0
+      }
+    )
+  }
 
-			this.setState({ secs: 0 })
-			this.tickMin()
-		}
-		if (millisecs > 99) {
-			this.setState({ millisecs: 0 })
-			this.tickSecs()
-		}	
+  componentDidMount() {
+    this.startWatch();
+  }
 
-	}
+  componentDidUpdate() {
+    const { millisecs, secs, minutes, stopped } = this.state
+    if (secs > 59) {
+
+      this.setState({ secs: 0 })
+      this.tickMin()
+    }
+    if (millisecs > 99) {
+      this.setState({ millisecs: 0 })
+      this.tickSecs()
+    }
+
+  }
 
 
 
-	render() {	
-		let{secs,millisecs,minutes}=this.state;
-		const zero= "0";		
-		if(millisecs<10){
-			millisecs=zero+millisecs;
-		}
-		if(secs<10){
-			secs=zero+secs;
-		}
-		if(minutes<10){
-			minutes=zero+minutes;
-		}
-		const time = `${minutes} : ${secs}.${millisecs}`;
-		return (<React.Fragment>
-			<h2>{time}</h2>
-			<div>
-				<button className="button-crono" onClick={this.toggleWatch}>Start</button>
-				<button className="button-crono" onClick={this.clearWatch}>Clear</button>
-			</div>
-		</React.Fragment>
-		)
-	}
+  render() {
+    let { secs, millisecs, minutes } = this.state;
+    const zero = "0";
+    if (millisecs < 10) {
+      millisecs = zero + millisecs;
+    }
+    if (secs < 10) {
+      secs = zero + secs;
+    }
+    if (minutes < 10) {
+      minutes = zero + minutes;
+    }
+    const time = `${minutes} : ${secs}.${millisecs}`;
+    return (<React.Fragment>
+      <h2>{time}</h2>
+      <div>
+        <button className="button-crono" onClick={this.toggleWatch}>Start</button>
+        <button className="button-crono" onClick={this.clearWatch}>Clear</button>
+      </div>
+    </React.Fragment>
+    )
+  }
 }
 
 export default Timer;
