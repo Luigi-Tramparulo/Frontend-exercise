ac614dd0568da226e81f5173c7951a9253b02ca5
Luigi-Tramparulo <luigi.tramparulo@abstract-technology.com>
2019-11-11 18:30:05 +0100

    feat: addnew component clock

---
+++ src/components/clock/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import style from './clock.module.scss'
+
+
+class Time extends Component {
+  constructor(props) {
+    super(props)
+
+    this.toggleWatch= this.toggleWatch.bind(this);
+
+    this.state = {
+      timestamp: Date.now(),
+      date: new Date(),
+      timezone: 1,
+      stopped: false
+    }
+
+  }
+
+  tick = () => {
+    this.setState((PrecState, props) => {
+      return {
+        timestamp: PrecState.timestamp + props.secs * 1000
+      }
+    }
+    )
+  }
+  startWatch() {
+    this.interval = setInterval(this.tick, this.props.secs * 1000);
+  }
+  toggleWatch(e){
+    this.setState((state)=>{
+        this.state.stopped ? this.startWatch() : clearInterval(this.interval);
+        return {stopped : ! state.stopped}
+    })
+}
+
+componentDidMount() {
+    this.startWatch();
+}
+componentWillMount() {
+    clearInterval(this.interval);
+}
+
+  render() {
+
+    const d = new Date(this.state.timestamp);
+    const tempo = d.getTime() + this.props.timezone * 3600 * 1000;
+    const data = new Date(tempo);
+
+    return (
+      <div className={style.clock_container}>
+        <h2>In {this.props.country} is {data.toLocaleTimeString()}</h2>
+        <button onClick={this.toggleWatch}> {this.state.stopped ? 'Start' : 'Stop'}</button>
+      </div>
+    )
+
+  }
+
+}
+
+export default Time
--- src/routes/clock.js
+++ src/routes/clock.js
@@ -1,5 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react';
+import Time from '../components/clock';
 
-const Clock = () => <div>Clock</div>
+const clocks = [
+  {
+    secs: 1,
+    country: "Italy",
+    timezone: 0
+  },
+  {
+    secs: 2,
+    country: "Lisbona",
+    timezone: 1
+  },
+  {
+    secs: 3,
+    country: "Cuba",
+    timezone: -6
+  }
+];
+
+class Clock extends Component {
+  getClocks() {
+    return clocks.map((clock) => {
+     return <Time secs={clock.secs} country={clock.country} timezone={clock.timezone} />
+   });
+  }
+  render() {
+    return (
+      <div>
+        {this.getClocks()}
+      </div>
+    );
+  }
+}
 
 export default Clock;
