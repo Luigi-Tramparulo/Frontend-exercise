3370aac154ba918feb9348262ba7c0525115d502
Luigi-Tramparulo <luigi.tramparulo@abstract-technology.com>
2019-11-12 18:34:59 +0100

    style: create new clock

--- src/components/clock/index.js
+++ src/components/clock/index.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react'
-import style from './clock.module.scss'
+import React, { Component } from 'react';
+import './clock.module.scss';
+import style from './clock.module.scss';
+
 
 
 class Time extends Component {
@@ -17,6 +19,7 @@ class Time extends Component {
 
   }
 
+
   tick = () => {
     this.setState((PrecState, props) => {
       return {
@@ -27,6 +30,7 @@ class Time extends Component {
   }
   startWatch() {
     this.interval = setInterval(this.tick, this.props.secs * 1000);
+
   }
   toggleWatch(e) {
     this.setState((state) => {
@@ -43,17 +47,40 @@ class Time extends Component {
     clearInterval(this.interval);
   }
 
+
+
   render() {
+
 
     const d = new Date(this.state.timestamp);
     const tempo = d.getTime() + this.props.timezone * 3600 * 1000;
     const data = new Date(tempo);
+    let sec=45;
 
     return (
+      <React.Fragment>
       <div className={style.clock_container}>
         <h2>In {this.props.country} is {data.toLocaleTimeString()}</h2>
         <button onClick={this.toggleWatch}> {this.state.stopped ? 'Start' : 'Stop'}</button>
       </div>
+      <div className={style.clock}>
+      <div className={`${style.hand} ${style.hour}`}></div>
+      <div className={`${style.hand} ${style.minutes}`}></div>
+      <div style={{transform: `rotate(${sec}deg)`}} className={`${style.hand} ${style.sec}`}></div>
+      <div className={`${style.number} ${style.number1}`}>1</div>
+      <div className={`${style.number} ${style.number2}`}>2</div>
+      <div className={`${style.number} ${style.number3}`}>3</div>
+      <div className={`${style.number} ${style.number4}`}>4</div>
+      <div className={`${style.number} ${style.number5}`}>5</div>
+      <div className={`${style.number} ${style.number6}`}>6</div>
+      <div className={`${style.number} ${style.number7}`}>7</div>
+      <div className={`${style.number} ${style.number8}`}>8</div>
+      <div className={`${style.number} ${style.number9}`}>9</div>
+      <div className={`${style.number} ${style.number10}`}>10</div>
+      <div className={`${style.number} ${style.number11}`}>11</div>
+      <div className={`${style.number} ${style.number12}`}>12</div>
+      </div>
+      </React.Fragment>
     )
 
   }
