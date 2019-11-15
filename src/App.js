c9f4272497bb2a4085809055c7f1583b603d03ac
Luigi-Tramparulo <luigi.tramparulo@abstract-technology.com>
2019-11-15 11:55:23 +0100

    feat: add component film, add routes to film, and add style active to link menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Clock from './routes/clock';
 import Timer from './routes/timer';
+import Film from './routes/film';
 
-import { Link, BrowserRouter, Switch, Route } from 'react-router-dom';
+import { NavLink, BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
@@ -14,20 +15,26 @@ function App() {
       <BrowserRouter>
         <header className="App-header">
 
+        <nav>
           <ul>
             <li>
-              <Link to="/clock" > Go to clock </Link>
+              <NavLink to="/clock" activeStyle={{ fontWeight: "bold", color: "black" }}>Go to clock</NavLink>
             </li>
             <li>
-              <Link to="/timer" > Go to Timer </Link>
+              <NavLink to="/timer" activeStyle={{ fontWeight: "bold", color: "black" }} > Go to Timer </NavLink>
+            </li>
+            <li>
+              <NavLink to="/film" activeStyle={{ fontWeight: "bold", color: "black" }} > Go to Film </NavLink>
             </li>
           </ul>
+          </nav>
 
         </header>
         <div>
           <Switch>
             <Route exact path="/clock" component={Clock} />
             <Route exact path="/timer" component={Timer} />
+            <Route exact path="/film" component={Film} />
           </Switch>
         </div>
       </BrowserRouter>
---
+++ src/routes/film.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Film extends Component {
+
+  render() {
+    return (
+      <div>
+        Film
+      </div>
+    );
+  }
+}
+
+export default Film;
