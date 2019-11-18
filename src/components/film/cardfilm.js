82b1769197222d393279f6d562157e6f9f797662
Luigi-Tramparulo <luigi.tramparulo@abstract-technology.com>
2019-11-18 17:14:58 +0100

    refactor: change text in button and added value to alt attribute on img

--- src/components/film/cardfilm.js
+++ src/components/film/cardfilm.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from 'react';
 import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button
@@ -22,14 +22,14 @@ import {
 
 // export default cardFilm
 
-const CardFilm = ({title, source, year, runtime,plot,link}) => (
+const CardFilm = ({ title, source, year, runtime, plot, link }) => (
   <Card>
-    <CardImg top width="100%" src={source} alt="Card image cap" />
+    <CardImg top width="100%" src={source} alt={title} />
     <CardBody>
       <CardTitle className="font-weight-bold">{title}</CardTitle>
       <CardSubtitle className="font-weight-bold text-secondary">{`Year:${year} Runtime${runtime}`}</CardSubtitle>
       <CardText>{plot}</CardText>
-      <Button className="btn-dark" href={`https://www.imdb.com/title/${link}/`}>Button</Button>
+      <Button className="btn-dark" href={`https://www.imdb.com/title/${link}/`}>Vedi i dettagli</Button>
     </CardBody>
   </Card>
 )
--- src/components/film/index.js
+++ src/components/film/index.js
@@ -1,64 +1,66 @@
-import React,{Component}  from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import CardFilm from './cardfilm';
 
 // Attraverso le props passo il parametro per la chiave dell'apikey
 
-  class FilmCards extends Component {
+class FilmCards extends Component {
 
-    constructor(props){
-      super(props)
-      this.state={
-        title:'title',
-        source:'source',
-        year: 'year',
-        runtime: 'runtime',
-        plot: 'plot',
-        link: 'link'
-      }
-    }
-// Aggiorno lo stato del titolo e del link esterno per la copertina destrutturalizzando l'oggetto data(risposta di axios al server)
-      requiredFilm = (namefilm) =>{
-      return(
-
-      axios.get(`http://www.omdbapi.com/?apikey=ac9b1a49&t=${namefilm}`)
-      .then(({data}) => {
-        this.setState( ({
-          title: data["Title"],
-          source: data["Poster"],
-          year: data["Year"],
-          runtime: data["Runtime"],
-          plot: data["Plot"],
-          link: data.imdbID
-        }))}
-      )
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .finally(function () {
-        // always executed
-      })
-      )
-    }
-    componentDidMount(){
-      this.requiredFilm(this.props.namefilm)
-    }
-
-
-    render() {
-
-     const {title,source,year, runtime, plot,link} =this.state;
-
-     // passo alla funzione importata 2 parametri con titolo e link esterno per la copertina
-
-      return (
-       <div className="col">
-       <CardFilm source={source} title={title} year={year} runtime={runtime} plot={plot} link={link}/>
-       </div>
-      );
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: 'title',
+      source: 'source',
+      year: 'year',
+      runtime: 'runtime',
+      plot: 'plot',
+      link: 'link'
     }
   }
+
+  // Aggiorno lo stato del titolo e del link esterno per la copertina destrutturalizzando l'oggetto data(risposta di axios al server)
+
+  requiredFilm = (namefilm) => {
+    return (
+
+      axios.get(`http://www.omdbapi.com/?apikey=ac9b1a49&t=${namefilm}`)
+        .then(({ data }) => {
+          this.setState(({
+            title: data["Title"],
+            source: data["Poster"],
+            year: data["Year"],
+            runtime: data["Runtime"],
+            plot: data["Plot"],
+            link: data.imdbID
+          }))
+        }
+        )
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        .finally(function () {
+          // always executed
+        })
+    )
+  }
+  componentDidMount() {
+    this.requiredFilm(this.props.namefilm)
+  }
+
+  render() {
+
+    const { title, source, year, runtime, plot, link } = this.state;
+
+    // passo alla funzione importata 5 parametri con titolo e link esterno per la copertina
+
+    return (
+      <div className="col">
+        <CardFilm source={source} title={title} year={year} runtime={runtime} plot={plot} link={link} />
+      </div>
+    );
+  }
+}
 
 
 export default FilmCards
