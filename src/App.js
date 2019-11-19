141ef0dfe946d8959d2ee6cf36970848ba837e63
Luigi-Tramparulo <luigi.tramparulo@abstract-technology.com>
2019-11-19 13:26:29 +0100

    refactor: clear code

--- src/App.js
+++ src/App.js
@@ -2,34 +2,31 @@ import React from 'react';
 import Clock from './routes/clock';
 import Timer from './routes/timer';
 import Film from './routes/film';
-
 import { NavLink, BrowserRouter, Switch, Route } from 'react-router-dom';
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 function App() {
+
   return (
 
     <div className="App">
       <BrowserRouter>
         <header className="App-header">
-
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/clock" activeClassName="selected" >Go to clock</NavLink>
-            </li>
-            <li>
-              <NavLink to="/timer" activeClassName="selected" > Go to Timer </NavLink>
-            </li>
-            <li>
-              <NavLink to="/film" activeClassName="selected" > Go to Film </NavLink>
-            </li>
-          </ul>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/clock" activeClassName="selected" >Go to clock</NavLink>
+              </li>
+              <li>
+                <NavLink to="/timer" activeClassName="selected" > Go to Timer </NavLink>
+              </li>
+              <li>
+                <NavLink to="/film" activeClassName="selected" > Go to Film </NavLink>
+              </li>
+            </ul>
           </nav>
-
         </header>
         <div>
           <Switch>
