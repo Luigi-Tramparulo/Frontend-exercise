c0f2971ef9cf295d28b44b2222125e375de2f963
Ivan Di Luccio <ivan.diluccio@abstract-technology.com>
2019-11-22 17:32:18 +0100

    fix: showing film content without waiting a response

--- src/routes/film.js
+++ src/routes/film.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import Films from '../components/film/sheduleFilm'
 
- const Film = (props) => {
-
-  let {id} = props.match.params;
-
-  return (
-    <div>
-    <Films namefilm={id}/>
-    </div>
-  );
+ const Film = props => {
+  const {id} = props.match.params;
+  return <Films namefilm={id}/>
 }
-
 
 export default Film;
