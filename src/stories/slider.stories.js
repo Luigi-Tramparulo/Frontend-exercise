8ed03b2dd2cdbc93880caff9ec3382f511bd2b01
Luigi-Tramparulo <luigi.tramparulo@abstract-technology.com>
2019-12-02 12:18:31 +0100

    fix: slider.stories.js now work

--- src/stories/slider.stories.js
+++ src/stories/slider.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from "@storybook/react";
+import {BrowserRouter} from 'react-router-dom'
 import Slider from '../components/slider';
 
 storiesOf("Slide", module)
-.add("slide",() =>(<Slider />))
+.add("slide",() =>(<BrowserRouter><Slider /></BrowserRouter>))
